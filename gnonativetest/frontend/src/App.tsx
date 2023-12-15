b86b27e3c77b99363a061ab9e787caf285aeb0a8
moul <94029+moul@users.noreply.github.com>
2023-12-15 16:10:11 +0100

    chore: following the tutorial
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- gnonativetest/frontend/src/App.tsx
+++ gnonativetest/frontend/src/App.tsx
@@ -1,28 +1,29 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useState } from "react";
+import logo from "./assets/images/logo-universal.png";
+import "./App.css";
+import { useGno } from "./api/use-gno";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+    const [resultText, setResultText] = useState(
+        "Start GnoNative Kit on Wails to see result"
+    );
 
-    function greet() {
-        Greet(name).then(updateResultText);
+    const gno = useGno();
+
+    const onAction1 = () => {
+        gno.listKeyInfo().then((res) => setResultText(res.toString()));
     }
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
+            <img src={logo} id="logo" alt="logo" />
+            <div className="input-box"></div>
+            <div id="result" className="result">
+                {resultText}
             </div>
+            <button className="btn" onClick={onAction1}>Action 1</button>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
